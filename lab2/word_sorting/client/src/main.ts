370ff182230c81030f7134b8afb79221d730b946
ggodos <deadly536@gmail.com>
2024-03-02 10:45:09 +0500

    lab2.1 + pdf

---
+++ lab2/word_sorting/client/src/main.ts
@@ -0,0 +1,25 @@
+import "./style.css";
+
+const socket = new WebSocket("ws://localhost:8080/sort");
+
+const input = document.querySelector<HTMLTextAreaElement>("#input")!;
+
+const resultContainer = document.querySelector<HTMLDivElement>("#result")!;
+
+document
+  .querySelector<HTMLButtonElement>("#sort")!
+  .addEventListener("click", async () => {
+    socket.send(input!.value)
+  });
+
+
+socket.onmessage = (event) => {
+  const output = event.data!;
+  let words = output.split(" ");
+  
+  resultContainer.innerHTML = `
+    <ul class='list-item'>
+      ${words.map((word: string) => `<li>${word}</li>`).join("")}
+    </ul>
+  `
+}
---
+++ lab2/word_sorting/server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"sort"
+	"strings"
+
+	"golang.org/x/net/websocket"
+)
+
+func sortWords(textBlock string) []string {
+	words := strings.Fields(textBlock)
+
+	// sort words without repeat
+	wordsMap := make(map[string]bool)
+	for _, word := range words {
+		wordsMap[word] = true
+	}
+	uniqueWords := make([]string, 0, len(wordsMap))
+	for word := range wordsMap {
+		uniqueWords = append(uniqueWords, word)
+	}
+	sort.Strings(uniqueWords)
+	return uniqueWords
+}
+
+func EchoServer(ws *websocket.Conn) {
+	io.Copy(ws, ws)
+}
+
+func SortServer(ws *websocket.Conn) {
+	buf := make([]byte, 32*1024)
+	n, err := ws.Read(buf)
+	if err != nil {
+		return
+	}
+	words := string(buf[:n])
+	sortedWords := sortWords(words)
+	ws.Write([]byte(strings.Join(sortedWords, " ")))
+}
+
+// This example demonstrates a trivial echo server.
+func main() {
+	http.Handle("/echo", websocket.Handler(EchoServer))
+	http.Handle("/sort", websocket.Handler(SortServer))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
