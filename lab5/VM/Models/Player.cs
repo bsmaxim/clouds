b76bb9888e0eee2932d451ba9164b6f24af62de1
ggodos <deadly536@gmail.com>
2024-03-04 22:32:17 +0500

    lab5 - полуприкол

--- lab5/VM/Models/Player.cs
+++ lab5/VM/Models/Player.cs
@@ -5,12 +5,13 @@ namespace VM.Models;
 public class Player
 {
     public int Id { get; set; }
-    [StringLength(30)]
+    [StringLength(30), Required]
     public required string Name { get; set; }
     public PlayerClass PlayerClass { get; set; }
     [EmailAddress]
     public string? Email { get; set; }
     public int Level { get; set; }
+    [Required]
     public required string Position { get; set; }
 }
 
--- lab5/VM/Program.cs
+++ lab5/VM/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<GameContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,6 +24,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
